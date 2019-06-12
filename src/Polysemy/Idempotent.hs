5ec6c519c61d6b26e2e069af47a357be2c13429a
Sandy Maguire <sandy@sandymaguire.me>
2019-06-12 17:51:08 -0400

    Idempotency effect

---
+++ src/Polysemy/Idempotent.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Polysemy.Idempotent
+  ( Idempotent
+  , idempotently
+  , Idempotency ()
+  , newIdempotency
+  , runIdempotentInIO
+  ) where
+
+import           Control.Exception (evaluate)
+import           Data.Dynamic
+import           Data.IORef
+import qualified Data.IntMap as M
+import           Data.Maybe
+import           Polysemy
+import           System.Mem.StableName
+
+data Idempotent m a where
+  Idempotently :: Typeable a => key -> m a -> Idempotent m a
+
+makeSem ''Idempotent
+
+data Idempotency = Idempotency (IORef (M.IntMap Dynamic))
+
+newIdempotency :: IO Idempotency
+newIdempotency = Idempotency <$> newIORef M.empty
+
+
+makeStableKey :: a -> IO Int
+makeStableKey a = do
+    st <- makeStableName a
+    return $ hashStableName st
+
+
+runIdempotentInIO
+    :: Member (Lift IO) r
+    => Idempotency
+    -> Sem (Idempotent ': r) a
+    -> Sem r a
+runIdempotentInIO idem@(Idempotency ref) = interpretH $ \case
+  Idempotently k m -> do
+    inspect' <- inspect <$> getInspectorT
+    (key, v) <- sendM $ (,) <$> makeStableKey k
+                            <*> readIORef ref
+
+    case M.lookup key v of
+      Nothing -> do
+        m' <- runT m
+        fa <- raise $ runIdempotentInIO idem m'
+        a <- sendM $ evaluate $ fromMaybe (error "non-pure idempotency block") $ inspect' fa
+        sendM . modifyIORef ref $ M.insert key $ toDyn a
+        pure fa
+      Just a -> pureT $ fromMaybe (error "idempotency collision") $ fromDynamic a
+
